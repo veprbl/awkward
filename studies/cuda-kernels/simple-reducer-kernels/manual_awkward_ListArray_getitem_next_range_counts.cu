de5fa926d8bbe169973da1805cee761033f42dbb
trickarcher <anishbiswas271@gmail.com>
2020-12-11 18:59:41 +0530

    Added another reducer kernel. Is using static_cast in kernels a good idea?

---
+++ studies/cuda-kernels/simple-reducer-kernels/manual_awkward_ListArray_getitem_next_range_counts.cu
@@ -0,0 +1,93 @@
+// BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+#define FILENAME(line)          \
+  FILENAME_FOR_EXCEPTIONS_CUDA( \
+      "src/cuda-kernels/awkward_IndexedArray_numnull.cu", line)
+
+#include "awkward/kernels.h"
+#include "standard_parallel_algorithms.h"
+
+template <typename C>
+__global__ void
+fromoffsets_offsets_kernel(int64_t* fromoffsets_range_mask,
+                           const C* fromoffsets,
+                           int64_t lenstarts) {
+  int64_t thread_id = blockDim.x * blockIdx.x + threadIdx.x;
+
+  if (thread_id < lenstarts) {
+    fromoffsets_range_mask[thread_id] =
+        static_cast<int64_t>(fromoffsets[thread_id + 1]) -
+        static_cast<int64_t>(fromoffsets[thread_id]);
+  }
+}
+
+template <typename C>
+void
+total_sum_reducer(int64_t* total, const C* fromoffsets, int64_t lenstarts) {
+  int64_t* fromoffsets_range_mask;  // Couldn't figure out a better variable name
+
+  HANDLE_ERROR(
+      cudaMalloc((void**)&fromoffsets_range_mask, sizeof(int64_t) * lenstarts));
+
+  dim3 blocks_per_grid = blocks(lenstarts);
+  dim3 thread_per_block = threads(lenstarts);
+
+  fromoffsets_offsets_kernel<<<blocks_per_grid, thread_per_block>>>(
+      fromoffsets_range_mask, fromoffsets, lenstarts);
+
+  simple_reducer(total, fromoffsets_range_mask, lenstarts);
+
+  HANDLE_ERROR(cudaFree(fromoffsets_range_mask));
+}
+
+template <typename C>
+ERROR
+awkward_ListArray_getitem_next_range_counts(int64_t* total,
+                                            const C* fromoffsets,
+                                            int64_t lenstarts) {
+  total_sum_reducer(total, fromoffsets, lenstarts);
+  return success();
+}
+ERROR
+awkward_ListArray32_getitem_next_range_counts_64(int64_t* total,
+                                                 const int32_t* fromoffsets,
+                                                 int64_t lenstarts) {
+  return awkward_ListArray_getitem_next_range_counts<int32_t>(
+      total, fromoffsets, lenstarts);
+}
+ERROR
+awkward_ListArrayU32_getitem_next_range_counts_64(int64_t* total,
+                                                  const uint32_t* fromoffsets,
+                                                  int64_t lenstarts) {
+  return awkward_ListArray_getitem_next_range_counts<uint32_t>(
+      total, fromoffsets, lenstarts);
+}
+ERROR
+awkward_ListArray64_getitem_next_range_counts_64(int64_t* total,
+                                                 const int64_t* fromoffsets,
+                                                 int64_t lenstarts) {
+  return awkward_ListArray_getitem_next_range_counts<int64_t>(
+      total, fromoffsets, lenstarts);
+}
+
+//int
+//main() {
+//  uint32_t fromoffsets[] = {1, 0, 2, 3, 1, 2, 0, 0, 1, 1, 2, 3, 1,
+//                            2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+//
+//  uint32_t* d_fromoffests;
+//
+//  int64_t length = 3;
+//
+//  int64_t d_total;
+//
+//  HANDLE_ERROR(cudaMalloc((void**)&d_fromoffests, sizeof(uint32_t) * length));
+//  HANDLE_ERROR(cudaMemcpy(d_fromoffests,
+//                          fromoffsets,
+//                          sizeof(uint32_t) * length,
+//                          cudaMemcpyHostToDevice));
+//  HANDLE_ERROR(cudaMalloc((void**)&d_total, sizeof(int64_t)));
+//
+//  auto error = awkward_ListArrayU32_getitem_next_range_counts_64(
+//      &d_total, d_fromoffests, length);
+//}
